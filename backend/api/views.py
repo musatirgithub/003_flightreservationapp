8518c8cf95df36e240539552d64a15ee8272c84f
Mehmet Ugur SATIR <musatir@gmail.com>
2023-04-17 12:05:51 +0300

    permissions are added to flight view

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,6 +1,7 @@
 from rest_framework.viewsets import ModelViewSet
 from .models import Flight
 from .serializers import FlightSerializer
+from .permissions import IsAdminOrReadOnly
 
 # Create your views here.
 
@@ -8,3 +9,4 @@ from .serializers import FlightSerializer
 class FlightView(ModelViewSet):
     queryset = Flight.objects.all()
     serializer_class = FlightSerializer
+    permission_classes = [IsAdminOrReadOnly]
