514c66439d9f2858cd5ed669657dab655c77ab46
Mehmet Ugur SATIR <musatir@gmail.com>
2023-04-18 03:20:38 +0300

    FlightView get_queryset method is updated

--- backend/api/views.py
+++ backend/api/views.py
@@ -2,6 +2,7 @@ from rest_framework.viewsets import ModelViewSet
 from .models import Flight, Reservation
 from .serializers import FlightSerializer, ReservationsSerializer
 from .permissions import IsAdminOrReadOnly
+from datetime import date, datetime
 
 # Create your views here.
 
@@ -10,6 +11,23 @@ class FlightView(ModelViewSet):
     queryset = Flight.objects.all()
     serializer_class = FlightSerializer
     permission_classes = [IsAdminOrReadOnly]
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        now = datetime.now()
+        current_time = now.strftime('%H:%M:%S')
+        current_date = date.today()
+
+        if not self.request.user.is_staff:
+            queryset = Flight.objects.filter(departure_date__gt=current_date)
+            if Flight.objects.filter(
+                    departure_date=current_date):
+                query_2 = Flight.objects.filter(
+                    departure_date=current_date).filter(
+                    departure_time__gt=current_time)
+                queryset = queryset.union(query_2)
+
+        return queryset
 
 
 class ReservationView(ModelViewSet):
